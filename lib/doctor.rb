af5e7b295e27b8dc3fa9ddefa1e7754942ac9324
T S <taneymail@gmail.com>
2020-02-23 01:50:23 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,28 @@
+class Doctor
+  
+  attr_accessor :name
+  @@all = []
+ 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end 
+  
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
+  end
+  
+  def new_appointment (patient,date)
+    Appointment.new(date,patient,self)
+  end
+  
+  def patients
+   appointments.map {|appointment| appointment.patient}
+  end 
+end
