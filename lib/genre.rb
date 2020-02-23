8b2733775331235ab71fa1fa73a83da3533d8b40
T S <taneymail@gmail.com>
2020-02-23 01:40:56 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,23 @@
+class Genre 
+  
+  @@all=[]
+  
+  attr_accessor :name
+  def initialize(name)
+    @name=name 
+    @@all << self 
+  end 
+ 
+  def songs 
+   Song.all.select{ |song| song.genre==self}
+  end 
+ 
+ def self.all
+  @@all 
+ end 
+
+  def artists
+    songs.map{|song| song.artist}
+  end 
+
+end 
